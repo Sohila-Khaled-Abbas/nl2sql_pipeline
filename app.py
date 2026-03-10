251e1bef1c7e00c7e8fe7c86be9be622cb061bf1
Sohila Khaled <agro.sohila.k.abbas@gmail.com>
2026-03-11 00:28:19 +0200

    feat: update available Gemini models in the selection box.

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ with st.sidebar:
     else:
         db_uri_input = st.text_input("Database Connection URI", help="Example: postgresql://user:password@localhost:5432/db")
     
-    model_choice = st.selectbox("Gemini Model", ["gemini-2.0-flash", "gemini-1.5-pro"])
+    model_choice = st.selectbox("Gemini Model", ["gemini-2.0-flash", "gemini-2.0-pro-exp-02-05", "gemini-1.5-pro-latest", "gemini-1.5-flash-latest"])
     
     if st.button("Connect & Initialize"):
         if not api_key_input:
